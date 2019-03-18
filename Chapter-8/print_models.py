7c659ead49789e847e518e895da66caea59e154f
John Goure <johngoure@protonmail.com>
2019-03-17 19:05:54 -0500

    imported the functions directly into the model.

--- Chapter-8/print_models.py
+++ Chapter-8/print_models.py
@@ -1,7 +1,7 @@
-import printing_functions as pf
+from printing_functions import print_models, show_completed_models
 
 unprinted_designs = ['iphone case', 'robot pendent', 'dodecahedron']
 completed_models = []
 
-pf.print_models(unprinted_designs, completed_models)
-pf.show_completed_models(completed_models)
+print_models(unprinted_designs, completed_models)
+show_completed_models(completed_models)
